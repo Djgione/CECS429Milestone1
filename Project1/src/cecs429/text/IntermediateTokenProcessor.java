cb8bef54eb366f1ff02b740a8a6e3e2ed201c916
Djgione <thegiones13@sbcglobal.net>
2020-09-23 16:19:02 -0700

    implemented porter2Stemming correctly, tested

--- Project1/src/cecs429/text/IntermediateTokenProcessor.java
+++ Project1/src/cecs429/text/IntermediateTokenProcessor.java
@@ -1,37 +1,63 @@
 package cecs429.text;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.regex.*;
+
+import org.tartarus.snowball.SnowballStemmer;
 
 public class IntermediateTokenProcessor implements TokenProcessor {
 
-	private final Pattern mQuotes = Pattern.compile("[\'\"]");
-	private final Pattern mHyphen = Pattern.compile("[-]");
+	//private final Pattern mQuotes = Pattern.compile("["']");
+	//private final Pattern mHyphen = Pattern.compile("[-]");
 	@Override
-	public List<String> processToken(String token) {
+	public List<String> processToken(String token){
 		// TODO Auto-generated method stub
 		List<String> temp = new ArrayList<>();
 		
-		// Removes all nonalphanumeric from front of string
+		// Removes all non-alphanumeric from front of string
 		while(!Character.isLetterOrDigit(token.charAt(0)))
 		{
 			token = token.substring(1);
 		}
-		// Removes all nonalphanumeric from back of string
+		
+		// Removes all non-alphanumeric from back of string
 		while(!Character.isLetterOrDigit(token.charAt(token.length()-1)))
 		{
 			token = token.substring(0,token.length()-1);
 		}
-		// Replace all quotations with no space
-		mQuotes.matcher(token).replaceAll("");
+
+		token = token.replaceAll("[\"\']","");
+		
+		
 		// Set to lower case
 		token = token.toLowerCase();
 		// Replace all hyphens with no space and make extra string out of it
-		String noHyphen = mHyphen.matcher(token).replaceAll("");
 		
+		//Wont take as list and let u add on additional
+		List<String> splitWords = new ArrayList<>(Arrays.asList(token.split("-")));
+		token = token.replaceAll("-", "");
+		splitWords.add(token);
 		
-		return null;
+		try 
+		{
+			Class<?> stemClass = Class.forName("org.tartarus.snowball.ext." + "english" + "Stemmer");
+			SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
+
+			for(int i = 0; i < splitWords.size(); i++)
+			{
+				stemmer.setCurrent(splitWords.get(i));
+				stemmer.stem();
+				temp.add(stemmer.getCurrent());
+			}
+			
+		}
+		catch(Exception e)
+		{
+			
+		}
+		return temp;
 	}
 
 	
