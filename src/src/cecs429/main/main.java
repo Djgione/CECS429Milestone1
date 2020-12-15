33d94aa0116159a37d4da4316e40e7d925c47648
KermiteO <kermicheo@gmail.com>
2020-12-15 12:57:14 -0800

    Update main.java

--- src/src/cecs429/main/main.java
+++ src/src/cecs429/main/main.java
@@ -63,43 +63,7 @@ import java.util.Map.Entry;
 
 public class main {
 	public static void main(String[] args) throws IOException, Exception 
-	{
-//		System.out.println("Building index...");
-//
-//
-//		Indexer indexer = new Indexer(Paths.get(Constants.currentPath),"txt");
-//		indexer.index();
-//		//indexer.getIndex().print();
-//		System.out.println("\n...index built\n\n");
-//		DiskIndexWriter diskWriter = new DiskIndexWriter(Constants.currentPath);
-//		
-//
-//		diskWriter.writeIndex(indexer.getIndex(),Paths.get(Constants.currentPath).toAbsolutePath());
-//		diskWriter.writeKgramIndex(indexer.getKgramIndex(),Paths.get(Constants.currentPath).toAbsolutePath());
-//
-//		DiskInvertedIndex di=new DiskInvertedIndex(Constants.currentPath);
-//		List<String> vocab= di.getVocabulary();
-//		List<String> indexVocab = indexer.getVocabulary();
-//		//System.out.println("vocab.size(): " + vocab.size());
-//
-//		//System.out.println("indexer vocab size: " + indexVocab.size());
-//
-//		DiskKgramIndex dki=new DiskKgramIndex(Constants.currentPath);
-//
-//		indexer.setDiskIndex(di);
-//		indexer.setDiskKgram(dki);
-//		List<String> results = indexer.rankedQuery("*t * t*e");
-//
-//		for(String s : results)
-//		{
-//			System.out.println(s);
-//		}
-//
-//		di.closeandDeleteDB(Constants.currentPath);
-//		diskWriter.DeleteBinFiles(Constants.currentPath);
-//		dki.closeandDeleteDB(Constants.currentPath);
-//		diskWriter.DeleteKgramBinFiles(Constants.currentPath);
-//		
+	{		
 		RocchioClassification classifier = new RocchioClassification();
 		RocchioValuesModel model = new RocchioValuesModel();
 		
@@ -108,39 +72,27 @@ public class main {
 		model.setDistanceFromClassifications(classifier.calculateDistance(model));
 
 		
-		
-//		for(int i = 0; i < 3; i++)
-//		{
-//			List<String> keys = new ArrayList<>(model.getCentroidValues().get(i).keySet());
-//			for(int k = 0; k < 10; k++)
-//			{
-//				System.out.print(keys.get(k) + ": ");
-//				System.out.print(model.getCentroidValues().get(i).get(keys.get(k)) + " | ");
-//			}
-//
-//		}
-		
-        System.out.println("\n\npaper52 Rocchio:");
-        System.out.println("Components:");
-        
-        List<String> keys = new ArrayList<>(model.getMappedDisputedDocs().get(3).keySet());
-        System.out.print("<");
-        for(int i = 0; i < 30; i++)
-        {
-                System.out.print(model.getMappedDisputedDocs().get(3).get(keys.get(i)) + ", ");
-                if(i==20)
-                	System.out.println();
-        }
-        System.out.println(">");
-        System.out.println("Order:\nHamilton\nJay\nMadison");
-        System.out.println("Distances:");
-        
-        for(int i = 0; i < 3; i++)
-        {
-                System.out.println(model.getDistanceFromClassifications().get(3).get(i));
-        }
+		System.out.println("\n\npaper52 Rocchio:");
+		System.out.println("Components:");
 
-        System.out.println("\n" +model.getDisputedDocsClassificationsToString());
+		List<String> keys = new ArrayList<>(model.getMappedDisputedDocs().get(3).keySet());
+		System.out.print("<");
+		for(int i = 0; i < 30; i++)
+		{
+			System.out.print(model.getMappedDisputedDocs().get(3).get(keys.get(i)) + ", ");
+			if(i==20)
+				System.out.println();
+		}
+		System.out.println(">");
+		System.out.println("Order:\nHamilton\nJay\nMadison");
+		System.out.println("Distances:");
+
+		for(int i = 0; i < 3; i++)
+		{
+			System.out.println(model.getDistanceFromClassifications().get(3).get(i));
+		}
+
+		System.out.println("\n" +model.getDisputedDocsClassificationsToString());
         
         
 
@@ -149,69 +101,60 @@ public class main {
         
         
 		
-        Indexer indexer4 = new Indexer(Paths.get(Constants.disputedPath).toAbsolutePath(),"txt",0);
-        Indexer bigIndexer = new Indexer(Paths.get(Constants.allPath).toAbsolutePath(), "txt",0);
-        Indexer hamiltonIndexer = new Indexer(Paths.get(Constants.hamiltonPath).toAbsolutePath(),"txt",0);
-        Indexer jayIndex=new Indexer(Paths.get(Constants.jayPath).toAbsolutePath(),"txt",0);
-        Indexer madisonIndex=new Indexer(Paths.get(Constants.madisonPath).toAbsolutePath(),"txt",0);
-        indexer4.index();
-        bigIndexer.index();
-        
-//        
-//        System.out.println("Vocab Size: " + bigIndexer.getVocabulary().size());
-//        
-//        for(int i = 0; i < 30; i++)
-//        {
-//     	   System.out.println(bigIndexer.getVocabulary().get(i));
-//        }
-        
-        Index hamiltonI=hamiltonIndexer.index();
-        Index jayI=jayIndex.index();
-        Index madisonI=madisonIndex.index();
-        Sequence s;
-        System.out.println(madisonI.getVocabulary().size()+hamiltonI.getVocabulary().size());
-        
-        //1.get total no of documnets from corpus and total no of documents in each catagory 
-        //find N00- documents that dont have the term and are are not in the catagory.
-//		  2.N11- documents  documents that  have the term and in the catagory.
-//        3.N10- documents that have the term and are not in the catagory
-//        4.N01- documents that do not have the term and are in the catagory.
+		Indexer indexer4 = new Indexer(Paths.get(Constants.disputedPath).toAbsolutePath(),"txt",0);
+		Indexer bigIndexer = new Indexer(Paths.get(Constants.allPath).toAbsolutePath(), "txt",0);
+		Indexer hamiltonIndexer = new Indexer(Paths.get(Constants.hamiltonPath).toAbsolutePath(),"txt",0);
+		Indexer jayIndex=new Indexer(Paths.get(Constants.jayPath).toAbsolutePath(),"txt",0);
+		Indexer madisonIndex=new Indexer(Paths.get(Constants.madisonPath).toAbsolutePath(),"txt",0);
+		indexer4.index();
+		bigIndexer.index();
 
-//        Bayes b=new Bayes(hamiltonI,jayI,madisonI);
-        Indexer indexer=new Indexer(Paths.get(Constants.disputedPath).toAbsolutePath(),"txt",0);
-//        
-//        for(Document d:indexer.getCorpus().getDocuments())
-//        {
-//        	System.out.println("for document"+d.getTitle());
-//        	double h=(double)b.classify(d,hamiltonI);
-//        	double j=(double)b.classify(d,jayI);
-//        	double m=(double)b.classify(d,madisonI);
-//        	System.out.println("hamilton:"+"  "+ h);
-//        	System.out.println("jay"+"  "+ j);
-//        	System.out.println("madison:"+"  "+ m);
-//        	
-//        	double temp=max(h,j,m);
-//        	if(h==temp)System.out.print("predicted author - Hamilton");
-//        	else if(j==temp)System.out.print("predicted author - Jay");
-//        	else if(m==temp)System.out.print("predicted author - Madison");
-//        	
-//        	System.out.println("");
-//        	System.out.println("");
-//        	System.out.println("");
-//        	System.out.println("");
-//        	
-//        }
-	
 
-       List<Indexer> threeCorpuses = new ArrayList<>();
-       threeCorpuses.add(jayIndex);
-       threeCorpuses.add(madisonIndex);
-       threeCorpuses.add(hamiltonIndexer);
-       knnClassification knn = new knnClassification();
-       System.out.println("before knn");
-       
-       knn.knn(threeCorpuses,bigIndexer, indexer4, 5);
-        System.out.println("made it out of knn");
+		Index hamiltonI=hamiltonIndexer.index();
+		Index jayI=jayIndex.index();
+		Index madisonI=madisonIndex.index();
+		Sequence s;
+		System.out.println(madisonI.getVocabulary().size()+hamiltonI.getVocabulary().size());
+
+		//1.get total no of documnets from corpus and total no of documents in each catagory 
+		//find N00- documents that dont have the term and are are not in the catagory.
+	//		  2.N11- documents  documents that  have the term and in the catagory.
+	//        3.N10- documents that have the term and are not in the catagory
+	//        4.N01- documents that do not have the term and are in the catagory.
+
+	        Bayes b=new Bayes(hamiltonI,jayI,madisonI);
+		Indexer indexer=new Indexer(Paths.get(Constants.disputedPath).toAbsolutePath(),"txt",0);
+	       
+	        for(Document d:indexer.getCorpus().getDocuments())
+	        {
+	        	System.out.println("for document"+d.getTitle());
+	        	double h=(double)b.classify(d,hamiltonI);
+	        	double j=(double)b.classify(d,jayI);
+	        	double m=(double)b.classify(d,madisonI);
+	        	System.out.println("hamilton:"+"  "+ h);
+	        	System.out.println("jay"+"  "+ j);
+	        	System.out.println("madison:"+"  "+ m);
+	       	
+	        	double temp=max(h,j,m);
+	        	if(h==temp)System.out.print("predicted author - Hamilton");
+	        	else if(j==temp)System.out.print("predicted author - Jay");
+	        	else if(m==temp)System.out.print("predicted author - Madison");
+	       	
+	        	System.out.println("");
+	        	System.out.println("");
+	        	System.out.println("");
+	        	System.out.println("");
+	       	
+	        }
+
+
+	       List<Indexer> threeCorpuses = new ArrayList<>();
+	       threeCorpuses.add(jayIndex);
+	       threeCorpuses.add(madisonIndex);
+	       threeCorpuses.add(hamiltonIndexer);
+	       knnClassification knn = new knnClassification();
+
+	       knn.knn(threeCorpuses,bigIndexer, indexer4, 5);
         
     }
 	
